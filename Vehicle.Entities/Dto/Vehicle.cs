bac5fa4fd23d6a7919a8f4d730c03ad0506907d3
Ryan <RyanSimpson94@users.noreply.github.com>
2020-12-11 09:45:40 +0000

    Add files via upload

---
+++ Vehicle.Entities/Dto/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Vehicle.Entities.Dto
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int ManufacturerId { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public string DisplayName => $"{Manufacturer} {Name}";
+		public decimal Price { get; set; }
+		public string DisplayPrice => $"£{Price.ToString("#,##0.00")}";
+        public IList<Vehicle> Trims { get; set; }
+        public int Order { get; set; }
+        public string ImgUrl { get; set; }
+        public bool isActive { get; set; }
+    }
+}
---
+++ Vehicle.Entities/Dto/VehicleGroup.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Vehicle.Entities.Dto
+{
+    public class VehicleGroup
+    {
+        public int Id { get; set; }
+        public IList<Vehicle> Vehicles { get; set; }
+        public int Order { get; set; }
+    }
+}
